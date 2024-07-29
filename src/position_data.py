d408b8658fb0018aa82bb0dcdf275a22527c16c0
taishin <ok220259@oks.iput.ac.jp>
2024-07-29 13:51:52 +0900

    キー値を変更、追加

--- src/position_data.py
+++ src/position_data.py
@@ -1,6 +1,8 @@
 class PositionData:
-    dist_position = {"destination": [2.2, 0, 0, 0, 0, 0, 1],
-                     "temp_posi": [1, -1, 0, 0, 0, 0, 1],
+    dist_position = {"destination0": [2.2, 0, 0, 0, 0, 0, 1],
+                     "destination1":[],
+                     "destination2":[],
+                     "waitposition": [1, -1, 0, 0, 0, 0, 1],
                      "warehouse0": [0, 0, 0, 0, 0, 0, 1],
                      "warehouse1": [0.7, 0, 0, 0, 0, 0, 1]
                      }
